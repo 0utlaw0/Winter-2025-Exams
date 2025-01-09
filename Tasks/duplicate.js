2163b9a0f59dd7828a28b590bf1539286b75b6b6
0utlaw0 <polisukvadim510@gmail.com>
2025-01-09 22:02:45 +0200

    2nd change: delete else part and rename array and function parameter

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -3,10 +3,12 @@
 'use strict';
 
 const duplicate = (value, times) => {
-if (times <= 0) return [];
-const resultArr = Array(times);
-resultArr.fill(value, 0, times);
-return resultArr;
+  if (times <= 0) return [];
+  const resultArray = [];
+    for (let i = 0; i < times; i++) {
+      resultArray[i] = value;
+    }
+  return resultArray;
 };
 
 module.exports = duplicate;
