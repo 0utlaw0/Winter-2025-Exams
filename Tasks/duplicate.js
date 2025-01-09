5d0d0862747c64deb3d481f27f74e047695922c1
0utlaw0 <polisukvadim510@gmail.com>
2025-01-09 21:58:30 +0200

    1st change: add 'use strict' and add const

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -1,15 +1,12 @@
 // Refactor following solution
 // Return an array without duplicates
+'use strict';
 
-duplicate = (value, N) => {
-  if (N <= 0) return [];
-  else {
-    res = [];
-    for (let i = 0; i < N; i++) {
-      res[i] = value;
-    }
-    return res;
-  }
+const duplicate = (value, times) => {
+if (times <= 0) return [];
+const resultArr = Array(times);
+resultArr.fill(value, 0, times);
+return resultArr;
 };
 
 module.exports = duplicate;
