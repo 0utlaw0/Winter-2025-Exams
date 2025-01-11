1864ee2d3abce971d93d186728caad1740c888aa
0utlaw0 <polisukvadim510@gmail.com>
2025-01-12 01:00:26 +0200

    6th change: fix functions naming

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -2,9 +2,9 @@
 // Filter array by type name
 'use strict';
 
-const Filter = (array, type) => {
+const filter = (array, type) => {
   const sortedArray = array.filter((value) => typeof value === type);
   return sortedArray;
 };
 
-module.exports = Filter;
+module.exports = filter;
