2aba73c57c34041987dc3f99ffda7215868785d0
0utlaw0 <polisukvadim510@gmail.com>
2025-01-12 23:53:50 +0200

    3rd change: avoid mutation of function's parameter

--- Tasks/only.js
+++ Tasks/only.js
@@ -3,14 +3,12 @@
 'use strict';
 
 const copyOnly = (dict, ...toCopy) => {
+  const resultObj = {};
   const arrayOfKeys = Object.keys(dict);
   arrayOfKeys.forEach((value) => {
-    if (toCopy.includes(value)) {
-    } else {
-      delete dict[value];
-    }
+    if (toCopy.includes(value)) resultObj[value] = dict[value];
   });
-  return dict;
+  return resultObj;
 };
 
 module.exports = copyOnly;
